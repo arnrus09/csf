b95dee62bb8cce1885d3a2f743259ba50dad2c54
arnrus09 <russellarnold@michael-g-hunt.evergreen.edu>
2013-10-10 16:02:41 -0700

    homework1

---
+++ hw1.py
@@ -0,0 +1,51 @@
+# Name: Russ
+# Evergreen Login: arnrus09
+# Computer Science Foundations
+# Programming as a Way of Life
+# Homework 1
+
+# You may do your work by editing this file, or by typing code at the
+# command line and copying it into the appropriate part of this file when
+# you are done.  When you are done, running this file should compute and
+# print the answers to all the problems.
+
+import math                     # makes the math.sqrt function available
+
+
+###
+### Problem 1
+###
+
+print 'Problem 1 solution follows:'
+
+print (-5.86+math.sqrt(5.86**2-4*8.5408))/2,'or',(-5.86-math.sqrt(5.86**2-4*8.5408))/2
+# prints the values of x for the equation  x2-5.86 x+ 8.5408.
+
+
+###
+### Problem 2
+###
+
+print 'Problem 2 solution follows: '
+
+import hw1_test
+
+print 'a'
+print 'b'
+print 'c'
+print 'd'
+print 'e'
+print 'f'
+
+
+###
+### Problem 3
+###
+
+print "Problem 3 solution follows:"
+
+((a and b) or (not c) and not (d or e or f))
+
+
+
+
