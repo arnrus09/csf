59845d7247e180abe0f936798d0de5de7e4935c0
arnrus09 <russellarnold@Michael-G-Hunt.local>
2014-01-23 15:48:02 -0800

    lab 5

---
+++ gamepackage/Player.java
@@ -0,0 +1,10 @@
+package chap02b;
+
+public class Player {
+    int number = 0;
+    public void guess()
+    {
+        number = (int) (Math.random() * 100);
+        System.out.println("I'm guessing " + number);
+    }
+}
