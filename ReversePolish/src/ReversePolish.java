f06acc06b76b3a3849acc9d145141685c3c91a07
arnrus09 <arnrus09@evergreen.edu>
2014-02-11 14:51:37 -0800

    Update ReversePolish.java

--- ReversePolish/src/ReversePolish.java
+++ ReversePolish/src/ReversePolish.java
@@ -1,13 +1,45 @@
+import java.util.*;
 
-public class ReversePolish {
-
-	public ReversePolish() {
-		// TODO Auto-generated constructor stub
-	}
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+class ReversePolish{
+  static Stack<Integer> myStack = new Stack<Integer>();
+      
+  public static boolean isDigit(String str){  
+    try{  
+      int n = Integer.parseInt(str);  
+    }
+    catch(NumberFormatException nfe){  
+      return false;  
+    }  
+    return true;  
+  }
+    
+  public static void main(String... args){
+    for(String arg:args){
+      if(isDigit(arg)){
+        int n = Integer.parseInt(arg);
+        myStack.push(n);
+      }
+      else{
+    int op2 = myStack.pop();
+    int op1 = myStack.pop();
+    if (arg.equals("+")){
+      myStack.push(op1+op2);
+    }
+    else if (arg.equals("*")){
+      myStack.push(op1*op2);
+    }
+    else if (arg.equals("-")){
+      myStack.push(op1-op2);
+    }
+    else if (arg.equals("/")){
+      myStack.push(op1/op2);
+    }       
+        
+    }
+  }
+  System.out.println(myStack.peek());
 }
+}
+    
+    
+    
